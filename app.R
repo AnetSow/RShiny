880955f19108f37dade4d4b76caf8ff9821fde33
AnetSow <42640301+AnetSow@users.noreply.github.com>
2020-01-22 21:39:02 +0100

    Add files via upload
    New functionalities added:
    - creating, updateing and saving datatable with device's response (measurements)
    - plotting measurements based on stored data.
    Altered parameters' collecting function (formData()).
    Checking whether measurement if confirmed before it is read.

--- app.R
+++ app.R
@@ -1,128 +1,193 @@
-# setwd("C:/Users/Aneta/Projects/RShinyApp")
-
 library(shiny)
 library(shinyalert)
 library(serial)
 library(DT)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+rm(list=ls())
+
+saveData <- function(res) {
+  if (exists("responses")) {
+    print("Adding new measurements to data table...")
+    responses2 <<- data.frame(res, stringsAsFactors = FALSE)
+    responses2 <<- responses2 %>% separate(res, c("channel", "freq", "impedance", "re", "im"), extra='drop')
+    responses <<- rbind(responses, responses2)
+    
+  } else {
+    print("Creating data table with measurements...")
+    responses <<- data.frame(res, stringsAsFactors = FALSE)
+    print(responses)
+    
+    responses <<- responses %>% separate(res, c("channel", "freq", "impedance", "re", "im"), extra='drop')
+    print(responses)
+  }
+}
+
+loadData <- function() {
+  if (exists("responses")) {
+    responses
+  }
+}
+
+getDataValue <- function(i, j) {
+  stopifnot(exists("responses"))
+  responses[i, j]
+}
+
+setDataValue <- function(i, j, value) {
+  stopifnot(exists("responses"))
+  responses[i, j] <<- value
+  responses
+}
 
 
-# Defines UI for app
+fields <- c("channelChoice", "cycleNumber", "cmdChoice")
+
+
+
 ui <- shinyUI(fluidPage(
   
-  titlePanel("Mobi C bis"),
-
+  titlePanel("Mobi C bis v2"),
+  
   sidebarLayout(position = "right",
-    sidebarPanel(
-      
-      helpText("Use buttons below in order to connect or disconnect with the device."),
-      
-      # creates buttons to connect or disconnect
-      useShinyalert(),
-      actionButton("conButton", label="Connect"),
-      actionButton("disconButton", label="Disconnect"),
-
-      # hr(),
-      # # creates input for a command and executing button
-      # textInput("Command", "Enter a command:", " "), 
-      # actionButton("executeButton", "Execute"),
-      
-      hr(),
-      # creates checkbox for channel choice
-      checkboxGroupInput("channelChoice", 
-                         label = h5("Select channels to measure"), 
-                         choices = list("Channel 1" = 1, "Channel 2" = 2, 
-                                        "Channel 3" = 3, "Channel 4" = 4, 
-                                        "Channel 5" = 5, "Channel 6" = 6,
-                                        "Channel 7" = 7, "Channel 8" = 8),
-                         selected = 1),
-      
-      hr(),
-      # creates input for number of cycles to run (for each channel)
-      textInput("cycleNumber", "Number of measuring cycles", ""),
-      
-      hr(),
-      # creates input for measuring command choice
-      selectInput("cmdChoice", h5("Choose measuring command"), 
-                  choices = list("107" = 107, "110" = 110,
-                                 "114" = 114), selected = 107),
-      
-      # creates executing button
-      actionButton("startButton", label="Start")
-
-    ),
-    
-    mainPanel(
-      # h4("Used command"),
-      # # shows last used command
-      # verbatimTextOutput("Command"),
-      
-      # shows used parameters
-      # h4("Used parameters"), textOutput("paramsOut"),
-      # h4("Output as text"), textOutput("mobiOut"),
-      h4("Output as data table"), dataTableOutput("dataTableOut")
-    )
+            sidebarPanel(
+              
+              helpText("Use buttons below in order to connect or disconnect with the device."),
+        
+              useShinyalert(),
+              actionButton("conButton", label="Connect"),
+              actionButton("disconButton", label="Disconnect"),
+              
+              hr(),
+              checkboxGroupInput("channelChoice", 
+                                 label = h5("Select channels to measure"), 
+                                 choices = list("Channel 1" = 1, "Channel 2" = 2, 
+                                                "Channel 3" = 3, "Channel 4" = 4, 
+                                                "Channel 5" = 5, "Channel 6" = 6,
+                                                "Channel 7" = 7, "Channel 8" = 8),
+                                 selected = 1),
+              hr(),
+              textInput("cycleNumber", "Number of measuring cycles", "1"),
+              
+              hr(),
+              selectInput("cmdChoice", h5("Choose measuring command"), 
+                          choices = list("107" = 107, "110" = 110,
+                                         "114" = 114), selected = 107),
+        
+              actionButton("startButton", label="Start"),
+              actionButton("plotButton", label="Plot")
+              
+            ),
+            
+            mainPanel(
+              # h4("Output as text"), textOutput("mobiOut"),
+              h4("Output as data table"), DT::dataTableOutput("responses"),
+              h4("Plot"), plotOutput("plot")
+            )
   )
 ))
 
-# Defines server for app
 server <- shinyServer(function(input, output, session) {
   
-  # creates, opens and checks (dis)connection with the device
-  observeEvent(input$conButton, {
-    mobiConn <<- serialConnection("mobi", port="COM5", mode="115200,n,8,1")
-    open(mobiConn)
-    
-    connChecking <- function(mobiConn){
-      if (isOpen(mobiConn) == TRUE){ 
-        print("Successfully connected!")
-      } else {
-        print("Connection failed!")}
-    }
-    
-    shinyalert(connChecking(mobiConn))
-  })
+          observeEvent(input$conButton, {
+            mobiConn <<- serialConnection("mobi", port="COM5", mode="115200,n,8,1")
+            open(mobiConn)
+            
+            connChecking <- function(mobiConn){
+              if (isOpen(mobiConn) == TRUE){ 
+                print("Successfully connected!")
+              } else {
+                print("Connection failed!")}
+            }
+            
+            shinyalert(connChecking(mobiConn))
+          })
+          
+          observeEvent(input$disconButton, {
+            close(mobiConn)
+            
+            disconnChecking <- function(mobiConn){
+              if (isOpen(mobiConn) == FALSE){ 
+                print("Successfully disconnected!")
+              } else {
+                print("Disconnection failed!")}
+            }
+            
+            shinyalert(disconnChecking(mobiConn))
+          })
+          
+          formData <- reactive({
+            sapply(fields, function(x) input[[x]], simplify = FALSE)
+          })
+          
+          output$responses <- DT::renderDataTable({
+            req(input$startButton)
+            inputParameters <- isolate(formData())
+            
+            request <- paste(inputParameters$cmdChoice, "3", inputParameters$channelChoice, "1000 0\r\n", sep=" ")
+            
+            # write.serialConnection(mobiConn, "108 0\r\n") # otwarcie sesji
+            # read.serialConnection(mobiConn, "108 0\r\n")
+            
+            write.serialConnection(mobiConn, request)
+            
+            Sys.sleep(0.2)
+            
+            confirmation <- read.serialConnection(mobiConn, n = 0)
+            print(confirmation)
+            
+            if (grepl('016777200', confirmation) == TRUE) {
+              print("Measurement confirmed")
+              Sys.sleep(5)
+              res <- read.serialConnection(mobiConn, n = 0)
+              print(res)
+              res <- substr(res, 9, nchar(res))
+              print(res)
+              
+              saveData(res)
+              datatable(loadData(), rownames = FALSE)
+              
+              # write.serialConnection(mobiConn, "109 0\r\n") # zamkniecie sesji
+              # read.serialConnection(mobiConn, "109 0\r\n")
+              
+            } else {
+              print("Measurement not confirmed")
+            }
+          })
+          
+          proxy <- dataTableProxy("responses")
+          
+          observeEvent(input$responses_cell_edit, {
+            info <- input$responses_cell_edit
+            print(info)
+            i <- info$row
+            j <- info$col + 1
+            v <- info$value
+            
+            newValue <- DT:::coerceValue(v, getDataValue(i, j))
+            setDataValue(i, j, newValue)
+            DT::replaceData(proxy, loadData(), resetPaging = FALSE, rownames = FALSE)
+          })
+          
+          observeEvent(input$plotButton, {
+            toPlot <- loadData()
 
-  observeEvent(input$disconButton, {
-    close(mobiConn)
-    
-    disconnChecking <- function(mobiConn){
-      if (isOpen(mobiConn) == FALSE){ 
-        print("Successfully disconnected!")
-      } else {
-        print("Disconnection failed!")}
-    }
-    
-    shinyalert(disconnChecking(mobiConn))
-  })
+            toPlot[] <- lapply(toPlot, as.numeric)
+            print("Data toPlot") 
+            print(toPlot)
+            
+            output$plot <- renderPlot({
+              re = toPlot$re
+              im = toPlot$im
+              
+              ggplot(toPlot,  aes(x=re, y=im)) + 
+                  geom_point() +
+                  geom_line()
+            })
+        })
 
-  # executes the command with parameters entered by user - as a text output
-  request <- eventReactive(input$startButton, {
-    perm.vector <- paste(input$channelChoice,input$cycleNumber,input$cmdChoice, sep = " ")
-    print(perm.vector)
-    write.serialConnection(mobiConn, paste(perm.vector, "\r\n", sep=""))
-    # data manipulaton
-    Sys.sleep(0.2)
-    res <- read.serialConnection(mobiConn, n = 0)
-    res2 <- unlist(strsplit(res, " "))
-    m <- matrix(res2, ncol=length(res2))
-    df <- as.data.frame(m)
-    })
-
-  # displays command 
-  output$mobiOut <- renderText({
-    request()
-    Sys.sleep(0.2)
-    read.serialConnection(mobiConn, n = 0)
-  })
-  
-
-  # calls as a data table output
-  output$dataTableOut <- DT::renderDataTable({
-    request()}, options = list(
-    lengthMenu = list(c(5, 15, -1), c('5', '15', 'All')),
-    pageLength = 15)
-  )
-  
 })
 
 

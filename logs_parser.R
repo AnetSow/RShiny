d6b0f6d1900c747792243b838a187295c2dcecfe
AnetSow <42640301+AnetSow@users.noreply.github.com>
2020-01-30 11:35:51 +0100

    Add files via upload
    Parser finished.

--- logs_parser.R
+++ logs_parser.R
@@ -1,79 +1,138 @@
-# setwd("C:/Users/Aneta/Projects/RShinyApp")
-
 # Parsing Mobi C responses and SensDxApp logs.
 
 
-
 # Reading in a log-file
+
 processFile <- function(filepath) {
   con = file(filepath, "r")
-  
-  # data =  vector("list")
-  
-  while (TRUE) {
-    line = readLines(con, n = 1)
-
-    if (length(line) == 0) {
-      break
-    } else if (grepl('^#', line) == TRUE) {
-      iToRpt <- unlist(strsplit(line, " "))[3]
-      iToRpt <- unlist(strsplit(iToRpt, "="))[2]
-      iToRpt
-    } else if (grepl('^<<', line) == TRUE) {
-      out <- unlist(strsplit(line, " "))
-      out <- out[4:length(out)]
-      out
-    }
     
-    # data <- c(data, iToRpt, out)
+  linesInFile = readLines(con) 
+  # print(length(linesInFile))
+  
+  iToRpt_list <- list()
+  out_list <- list()
+  iToRpt_idx = 1
+  out_idx = 1
+
+  for (line in linesInFile){
+
+     if (length(line) == 0) {
+
+       break
+
+     } else if (grepl('^# iCmd', line) == TRUE) {
+       
+       iToRpt <- unlist(strsplit(line, " "))[3]
+       iToRpt <- unlist(strsplit(iToRpt, "="))[2]
+       # print(paste(" >>> iToRtp:", iToRpt, sep=" "))
+
+       iToRpt_list[[iToRpt_idx]] <- iToRpt
+       iToRpt_idx <- iToRpt_idx + 1
+
+     } else if (grepl('^<<', line) == TRUE) {
+
+       out <- unlist(strsplit(line, " "))
+       out <- out[4:length(out)]
+       out <- paste(out, collapse=" ")
+       # print(paste(" >>> out: ", out, sep=" "))
+       
+       out_list[[out_idx]] <- out
+       out_idx <- out_idx + 1
+     }
   }
-  
-  # return(data)
-  
-  return(list(iToRpt=iToRpt, out=out))
-  
+
+  data <- list(iToRpt_list, out_list)
+  names(data) <- c("iToRpt", "out")
+  return(data)
+
   close(con)
 }
 
 
 # Extracting channel number
+
 channelNumber <- function(out){
-  return(out[3])
+  channel_vec <- vector()
+  channel_idx <- 1
+  
+  for (i in 1:length(out)){
+    channel_vec[channel_idx] <- unlist(strsplit(out[[i]], " "))[3]
+    i = i + 1
+    channel_idx = channel_idx + 1
+  }
+  return(channel_vec)
 }
+
 
 # Extracting cmd number
+
 cmdNumber <- function(out){
-  cmd <- substr(out[1], 2, nchar(out[1]))
-  return(cmd)
+  cdm_vec <- vector()
+  cdm_idx <- 1
+  
+  for (i in 1:length(out)){
+    cdm_vec[cdm_idx] <- substr(unlist(strsplit(out[[i]], " "))[1], 2, nchar(unlist(strsplit(out[[i]], " "))[1]))
+    i = i + 1
+    cdm_idx = cdm_idx + 1
+  }
+  return(cdm_vec)
 }
+
+# cmdNumber(p$out)
+
 
 # Extracting frequency
+
 frequencyLevel <- function(out){
-  freq <- out[4]
-  return(freq)
+  freq_vec <- vector()
+  freq_idx <- 1
+  
+  for (i in 1:length(out)){
+    freq_vec[freq_idx] <- unlist(strsplit(out[[i]], " "))[4]
+    i = i + 1
+    freq_idx = freq_idx + 1
+  }
+  return(freq_vec)
 }
 
+
 # Extracting impedance
+
 impedanceRe  <- function(out){
-  z_re <- out[5]
-  return(z_re)
+  z_re_vec <- vector()
+  z_re_idx <- 1
+  
+  for (i in 1:length(out)){
+    z_re_vec[z_re_idx] <- unlist(strsplit(out[[i]], " "))[5]
+    i = i + 1
+    z_re_idx = z_re_idx + 1
+  }
+  return(z_re_vec)
 }
 
 impedanceIm  <- function(out){
-  z_im <- out[6]
-  return(z_im)
+  z_im_vec <- vector()
+  z_im_idx <- 1
+  
+  for (i in 1:length(out)){
+    z_im_vec[z_im_idx] <- unlist(strsplit(out[[i]], " "))[6]
+    i = i + 1
+    z_im_idx = z_im_idx + 1
+  }
+  return(z_im_vec)
 }
 
-# Creating data frame
+
+# Creating data frame with results
+
 log_df <- function(){
-  p <- processFile("C:/Users/Aneta/Projects/RShinyApp/A_rep2.log")
-  str(p)
+  p <- processFile(file.choose())
   
   channel <- channelNumber(p$out)
   frequency <- frequencyLevel(p$out)
-  cycle <- p$iToRpt
-  z_im <- impedanceIm(p$out)
+  cycle <- rev(unlist(p$iToRpt)) # Reverting cycle number
   z_re <- impedanceRe(p$out)
+  z_im <- impedanceIm(p$out)
 
   results <- data.frame(channel, frequency, z_re, z_im, cycle)
   print(results)
@@ -81,22 +140,3 @@ log_df <- function(){
 
 
 log_df()
-
-
-
-
-# # Reverting cycle number
-# revertCycle <- function(iToRpt){
-#   
-#   cycles <- seq(1, 7)
-#   names(cycles) <- seq(7, 1, by = -1)
-#   print(cycles)
-#   # iToRpt <- as.numeric(iToRpt)
-#   
-#   for(c in 1:length(cycles)){
-#     if (iToRpt == cycles[c]) {
-#       iToRpt <- c
-#     }
-#   }
-#   print(iToRpt)
-# }
